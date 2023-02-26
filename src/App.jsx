8acfadbabe9c3289faf57e3074cf309f77123336
LuiiVasquez <luivasque95@gmail.com>
2023-02-26 11:09:22 -0600

    created project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="font-bold underline">Hola que hace?</h1>
+    </div>
+  );
+}
+
+export default App;
